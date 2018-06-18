06a4dace7e467a1801be49545205a764376e92b5
Thorsten Bruns <bruns8234@familiebruns.de>
2018-06-18 22:29:08 +0200

    README.txt replaced with README.md Updates to library.json

--- HM Battery Monitor/module.php
+++ HM Battery Monitor/module.php
@@ -1,6 +1,11 @@
 <?
+
+if (!defined('IPS_BASE'))  { define("IPS_BASE", 10000); }
+if (!defined('VM_UPDATE')) { define("VM_UPDATE", IPS_BASE + 603); }
+
+
     // Klassendefinition
-    class ModulnameXYZ extends IPSModule {
+    class HomematicBatteriyMonitor extends IPSModule {
  
         // Der Konstruktor des Moduls
         // Überschreibt den Standard Kontruktor von IPS
@@ -16,23 +21,49 @@
             // Diese Zeile nicht löschen.
             parent::Create();
  
+			$this->RegisterPropertyInteger("LOWBAT_ID", 0);		// ID der Batterievariablen (bool, true=Bat.leer, false=Bat.voll)
+			$this->RegisterPropertyBoolean("LAST_STATE", 0);
         }
  
         // Überschreibt die intere IPS_ApplyChanges($id) Funktion
         public function ApplyChanges() {
             // Diese Zeile nicht löschen
             parent::ApplyChanges();
-        }
+			
+			$this->RegisterVariableInteger("STATE", "State");
+			$this->RegisterVariableString("FIRST_LOW", "First Alarm");
+			$this->RegisterVariableString("LAST_CHANGE", "Last Change");
+			
+			$lowbatID = $this->ReadPropertyInteger("LOWBAT_ID");
+			$this->RegisterMessage($lowbatID, VM_UPDATE);
+		}
  
-        /**
-        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
-        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
-        *
-        * ABC_MeineErsteEigeneFunktion($id);
-        *
-        */
-        public function MeineErsteEigeneFunktion() {
-            // Selbsterstellter Code
-        }
-    }
+		public function MessageSink($TimeStamp, $SenderID, $Message, $Data) {
+		
+			$this->SendDebug("MessageSink", "SenderID: ". $SenderID .", Message: ". $Message, 0);
+			
+			$sourceID = $this->ReadPropertyInteger("LOWBAT_ID");
+			if($sourceID == $SenderID) {
+				$this->HandeUpdate(GetValue($sourceID));
+				return;
+			}
+		}
+		
+		public function HandleUpdate($NewState) {
+			
+			$LastStateID = $this->GetIDForIdent("LAST_STATE");
+			$FirstLowID  = $this->GetIDForIdent("FIRST_LOW");
+			
+			$LastState = GetValue($LastStateID);
+			
+			if ($lastState == false && $NewState == true) {
+				$this->SendDebug("HandleUpdate", "Change from FULL to EMPTY battery state", 0);
+				// First Indication of a empty battery
+				SetValue($LastStateID, true);
+				SetValue($FirstLowID, date("d.m.Y"));
+				SetValue($State, 0);
+				return;
+			}
+			if ($lastState == false && 
+		}
 ?>
---
+++ README.md
@@ -0,0 +1,8 @@
+# HMUtilities
+
+Folgende Module sind im Repository enthalten:
+
+- __HMBatteryMonitor__ ([Dokumentation] (HMBatteryMonitor))
+  Überwachung des Batteriezustands für Homematik Batterie Aktoren.
+  Zeigt wann die Batteriemeldung das erste Mal aufgetreten ist und
+  wann der letzte Batteriewechsel stattgefunden hat.
